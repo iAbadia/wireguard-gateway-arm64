161773214218b0af7b0ac4b06f5595bbaf6b6c18
Inaki Abadia <i.abadiaosta@gmail.com>
2021-06-15 21:58:34 +0100

    Update entrypoint.sh form

--- README.md
+++ README.md
@@ -7,10 +7,15 @@ https://hub.docker.com/r/iabadia/wireguard-gateway-arm64
 - Make sure your config file doesn't have an ipv6 on teh AllowedIPs section
 
 ## Setup
+We need to create a macvlan as the gateway needs to have it's own mac address and IP.
+
+```
 docker network create -d macvlan \
  --subnet=192.168.1.0/24 --gateway=192.168.1.1 -o parent=eth0 pub_net
+ ```
 
 ## Run
+```
 docker run --name wireguard-gateway \
   --network pub_net \
   --cap-add NET_ADMIN \
@@ -18,4 +23,5 @@ docker run --name wireguard-gateway \
   --sysctl net.ipv4.conf.all.src_valid_mark=1 \
   --sysctl net.ipv6.conf.all.disable_ipv6=1 \
   --sysctl net.ipv4.ip_forward=1 \
-  -v /path/to/file/router.conf:/etc/wireguard/router.conf iabadia/wireguard-gateway-arm64:0.2
+  -v /path/to/file/router.conf:/etc/wireguard/router.conf iabadia/wireguard-gateway-arm64:latest
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,31 @@
 
 set -e
 
+# Check default route exists
 default_route_ip=$(ip route | grep default | awk '{print $3}')
 if [[ -z "$default_route_ip" ]]; then
     echo "No default route configured" >&2
     exit 1
 fi
 
+# Check wireguard config file exists
 configs=`find /etc/wireguard -type f -printf "%f\n"`
 if [[ -z "$configs" ]]; then
     echo "No configuration files found in /etc/wireguard" >&2
     exit 1
 fi
 
+# Define wg interface name from conf file name
 config=`echo $configs | head -n 1`
 interface="${config%.*}"
 
+# Check we provided the right <docker run --sysctl> params
 if [[ "$(cat /proc/sys/net/ipv4/conf/all/src_valid_mark)" != "1" ]]; then
     echo "sysctl net.ipv4.conf.all.src_valid_mark=1 is not set" >&2
+    exit 1
+fi
+if [[ "$(cat /proc/sys/net/ipv4/ip_forward)" != "1" ]]; then
+    echo "sysctl net.ipv4.ip_forwarding=1 is not set" >&2
     exit 1
 fi
 
@@ -29,36 +37,24 @@ wg-quick up $interface
 # IPv4 kill switch: traffic must be either (1) to the WireGuard interface, (2) marked as a WireGuard packet, (3) to a local address, or (4) to the Docker network
 docker_network="$(ip -o addr show dev eth0 | awk '$3 == "inet" {print $4}')"
 docker_network_rule=$([ ! -z "$docker_network" ] && echo "! -d $docker_network" || echo "")
-#iptables -I OUTPUT ! -o $interface -m mark ! --mark $(wg show $interface fwmark) -m addrtype ! --dst-type LOCAL $docker_network_rule -j REJECT
+iptables -I OUTPUT ! -o $interface -m mark ! --mark $(wg show $interface fwmark) -m addrtype ! --dst-type LOCAL $docker_network_rule -j REJECT
+
+# Gateway forwarding
 iptables -t nat -A POSTROUTING -o $interface -j MASQUERADE # Enable NAT
 iptables -A FORWARD -i eth0 -o $interface -j ACCEPT    # Allow traffic from eth0 to wg iface
 iptables -A FORWARD -i $interface -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT # Allow returning traffic
 iptables -A INPUT -i lo -j ACCEPT  # Allow loopback
 iptables -A INPUT -i eth0 -p icmp -j ACCEPT # Allow pinging the container
-# iptables -P FORWARD DROP    # Disable
-# iptables -P INPUT DROP      # all other
-# iptables -L                 # traffic
 
-
+# IPV6 DISABLED #
 # IPv6 kill switch: traffic must be either (1) to the WireGuard interface, (2) marked as a WireGuard packet, (3) to a local address, or (4) to the Docker network
-docker6_network="$(ip -o addr show dev eth0 | awk '$3 == "inet6" {print $4}')"
-if [[ "$docker6_network" ]]; then
-    docker6_network_rule=$([ ! -z "$docker6_network" ] && echo "! -d $docker6_network" || echo "")
-    ip6tables -I OUTPUT ! -o $interface -m mark ! --mark $(wg show $interface fwmark) -m addrtype ! --dst-type LOCAL $docker6_network_rule -j REJECT
-else
-    echo "Skipping IPv6 kill switch setup since IPv6 interface was not found" >&2
-fi
-
-# Support LOCAL_NETWORK environment variable, which was replaced by LOCAL_SUBNET
-if [[ -z "$LOCAL_SUBNET" && "$LOCAL_NETWORK" ]]; then
-    LOCAL_SUBNET=$LOCAL_NETWORK
-fi
-
-if [[ "$LOCAL_SUBNET" ]]; then
-    echo "Allowing traffic to local subnet ${LOCAL_SUBNET}" >&2
-    ip route add $LOCAL_SUBNET via $default_route_ip
-    iptables -I OUTPUT -d $LOCAL_SUBNET -j ACCEPT
-fi
+# docker6_network="$(ip -o addr show dev eth0 | awk '$3 == "inet6" {print $4}')"
+# if [[ "$docker6_network" ]]; then
+#     docker6_network_rule=$([ ! -z "$docker6_network" ] && echo "! -d $docker6_network" || echo "")
+#     ip6tables -I OUTPUT ! -o $interface -m mark ! --mark $(wg show $interface fwmark) -m addrtype ! --dst-type LOCAL $docker6_network_rule -j REJECT
+# else
+#     echo "Skipping IPv6 kill switch setup since IPv6 interface was not found" >&2
+# fi
 
 shutdown () {
     wg-quick down $interface
